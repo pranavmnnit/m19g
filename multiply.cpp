33619bfded732338f5fec61ca4d3f7dfa164ddc1
Pranav <pranav.agrawal1@oyorooms.com>
2018-01-10 02:29:11 +0530

    changes

--- multiply.cpp
+++ multiply.cpp
@@ -3,12 +3,12 @@
 
 extern "C" {
 	extern void multiply_js(double x, double y);
-}
 
-int multiply_cpp(double x, double y) {
-	// call js method
-	double multiply_result = EM_ASM_({
-									return multiply_js($0, $1);
-							 }, x, y);
-	return multiply_result;
+	int multiply_cpp(double x, double y) {
+		// call js method
+		double multiply_result = EM_ASM_({
+										return multiply_js($0, $1);
+								 }, x, y);
+		return multiply_result;
+	}
 }
